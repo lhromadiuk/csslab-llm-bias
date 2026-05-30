aa05b78d4ccde39acf17f0645009c010332fd972
Mara Ludwig <maraludwig@MacBook-Pro-von-Mara.local>
2026-05-30 11:30:04 +0200

    no comment

--- bias_calculation/bias_calculation.py
+++ bias_calculation/bias_calculation.py
@@ -1,18 +1,49 @@
 import pandas as pd
 import numpy as np
 
+import os
+import sys
+
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+
+
+parent_dir = os.path.abspath(os.path.join(script_dir, ".."))
+
+if parent_dir not in sys.path:
+    sys.path.append(parent_dir)
+
 from vllm_runner import load_model, run_prompt, generate_from_messages
-from bias_calculation.prompt_loader import load_prompts
+from prompt_loader import load_prompts
 from vllm import SamplingParams
 
-prompts_list = load_prompts("prompts.txt", mode="survey")
 
 
-llm=load_model()
+if __name__ == "__main__":
 
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    prompts_path = os.path.join(script_dir, "..", "prompts.txt")
+    prompts_list = load_prompts(prompts_path, mode="lines")
 
-for i in range(2):
+    llm=load_model()
 
-    print(run_prompt(llm, prompt=prompts_list[0], max_tokens=80, temperature=1.0))
+    antworten = []
+
+    for i in range(2):
+
+        messages = [
+        {
+            "role": "system", 
+            "content": "Du bist ein automatisches System, antworte ausschließlich mit einer einzigen ziffer."
+        },
+
+        {
+            "role": "user", 
+            "content": prompts_list[i] 
+        }
+        ]
+
+        antwort = generate_from_messages(llm, max_tokens=5, temperature=1.0, messages=messages)
+        print(antwort)
 
 
